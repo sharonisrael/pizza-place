a1b22eb5cc2c2a6a4a4f618e3c4a407ff057a8c0
Sharon Israel <avisar@hotmail.com>
2020-10-03 15:58:28 +0300

    Updates

---
+++ precache-manifest.4f46b3e52e452bd8d08536a943b9761c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f3d3c0cc43deba66fad5df2cad9e952",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4868bf1325dd5ffd846",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "b4868bf1325dd5ffd846",
+    "url": "/static/js/2.8b6d3d79.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8b6d3d79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a49a5de685e75c72e7a",
+    "url": "/static/js/main.e617e9f1.chunk.js"
+  },
+  {
+    "revision": "84e3ffb78ab816cc4318",
+    "url": "/static/js/runtime-main.2c4cbded.js"
+  }
+]);
